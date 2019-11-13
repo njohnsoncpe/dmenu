718425a1e892093f95651868da563b2567b13091
njohnsoncpe <noah_johnson@uri.edu>
2019-11-13 17:47:45 +0000

    added pywal import support

--- config.h
+++ config.h
@@ -7,13 +7,11 @@ static const char *fonts[] = {
 	"monospace:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+
+//replace <USER> with your username 
+//#include "/home/<USER>/.cache/wal/colors-wal-dmenu.h"
+#include "/home/noah/.cache/wal/colors-wal-dmenu.h"
+
 static unsigned int lines      = 0;
 
 /*
